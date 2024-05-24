2c14dbb6e185f0660e856f80f8d7f702b58e973f
Bashlychev <gbashlychev@gmail.com>
2024-05-24 18:06:14 +0500

    anniversary fix

--- Documents/SixSixSix/src/BoxInterface.java
+++
@@ -1,9 +0,0 @@
-interface BoxInterface {
-    void addProduct(Product product);
-    void removeProduct(int index);
-    double getTotalWeight();
-    double getTotalPrice();
-    void displayAllProducts();
-    void optimizeWeight(double targetWeight);
-    void optimizePrice(double targetPrice);
-}
