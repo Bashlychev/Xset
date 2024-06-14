2d40122853603a036b7c9228d0fd305ae2385a58
Bashlychev <gbashlychev@gmail.com>
2024-06-14 18:03:09 +0500

    non proper version

--- Documents/23/src/main/java/com/example/demo/RequestController.java
+++ Documents/23/src/main/java/com/example/demo/RequestController.java
@@ -9,14 +9,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @RestController
-@RequestMapping(value = "/app/v1")
+@RequestMapping("/app/v1")
 public class RequestController {
 
-    @GetMapping(value = "/getRequest")
+    @GetMapping("/getRequest")
     public ResponseEntity<String> getRequest(@RequestParam int id, @RequestParam String name) throws IOException, InterruptedException {
-        if (id <= 10 || name.length() <= 5) {
+        if (id <= 10) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Internal Server Error: Invalid id or name length");
+                    .body("Неверный параметр: id должен быть больше 10");
+        }
+
+        if (name.length() <= 5) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Неверный параметр: Длина имени должна быть больше 5");
         }
 
         if (id > 10 && id < 50) {
@@ -25,7 +30,7 @@ public class RequestController {
             Thread.sleep(500);
         }
 
-        String response = new String(Files.readAllBytes(Paths.get("target/classes/getAnswer.txt")));
+        String response = new String(Files.readAllBytes(Paths.get("src/main/resources/getAnswer.txt")));
         response = response.replace("{id}", String.valueOf(id))
                 .replace("{name}", name);
 
@@ -36,15 +41,15 @@ public class RequestController {
     public ResponseEntity<String> postRequest(@RequestBody PostRequestBody requestBody) throws IOException {
         if (requestBody.getName().isEmpty() || requestBody.getSurname().isEmpty()) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Internal Server Error: Name or surname cannot be empty");
+                    .body("Internal Server Error: Имя или фамилия не могут быть пустыми");
         }
 
-        String response = new String(Files.readAllBytes(Paths.get("target/classes/postAnswer.txt")));
+        String response = new String(Files.readAllBytes(Paths.get("src/main/resources/postAnswer.txt")));
         Integer age = requestBody.getAge() == null ? 123 : requestBody.getAge();
         response = response.replace("{name}", requestBody.getName())
                 .replace("{surname}", requestBody.getSurname())
                 .replace("{age}", String.valueOf(age))
-                .replace("{age}*2", String.valueOf(age * 2));
+                .replace("2age", String.valueOf(requestBody.getAge() * 2));
         return ResponseEntity.ok(response);
     }
 }
